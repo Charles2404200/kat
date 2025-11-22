46ceca5a69126a2a2ad0e595ff28f13a9cc67858
Charles2404200 <163487573+Charles2404200@users.noreply.github.com>
2025-11-23 01:45:53 +0700

    Remove syntax

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,16 +21,16 @@ export default function Navbar() {
         <div className="collapse navbar-collapse justify-content-end" id="navbarMenu">
           <ul className="navbar-nav align-items-center gap-2">
             <li className="nav-item">
-              <a className="nav-link active" href="/">🏠 Trang chủ</a>
+              <a className="nav-link active" href="/"> Trang chủ</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/buy">🎟️ Mua vé</a>
+              <a className="nav-link" href="/buy"> Mua vé</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/checkin">✅ Check-in</a>
+              <a className="nav-link" href="/checkin"> Check-in</a>
             </li>
             <li className="nav-item">
-  <a className="nav-link" href="/service-check">🍔 Service Check</a>
+  <a className="nav-link" href="/service-check"> Service Check</a>
 </li>
             <li className="nav-item">
               <a className="btn btn-warning fw-bold ms-2" href="/admin">
